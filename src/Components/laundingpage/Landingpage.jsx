1c3d21249bc0f5a686ce4ff4c67ddbc5058a19b1
sriram <sriram3720@gmail.com>
2022-09-20 19:37:51 +0530

    landing page

---
+++ src/Components/laundingpage/Landingpage.jsx
@@ -0,0 +1,21 @@
+ import React, { Component } from 'react'
+ import './landingpg.css';
+class Landingpage extends Component  {
+    render(){
+        return(
+            <div>
+                <div className='Landingpage-f'>
+                    <div className="grid grid-item1"></div>
+                    <div className="grid grid-item1"></div>
+                    <div className="grid grid-item1"></div>
+                    <div className="grid grid-item1"></div>
+                    <div className="grid grid-item1"></div>
+                    <div className="grid grid-item1"></div>
+                </div>
+            </div>
+        )
+    }
+    
+}
+
+export  default Landingpage;
