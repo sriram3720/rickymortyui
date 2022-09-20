ad5e4757821e0b6162b9026fa7f18b99e911d568
sriram <sriram3720@gmail.com>
2022-09-20 22:09:55 +0530

    css

--- src/Components/laundingpage/Landingpage.jsx
+++ src/Components/laundingpage/Landingpage.jsx
@@ -1,19 +1,67 @@
  import React, { Component } from 'react'
  import './landingpg.css';
 class Landingpage extends Component  {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items:[],
+            isLoaded: false,
+        }           
+    }
+
+    componentDidMount(){
+        fetch('https://rickandmortyapi.com/api/character').then(res=>res.json()
+        .then(res =>{
+            console.log(res);
+            this.setState({
+                isLoaded: true,
+                items:res.results,
+            })
+        }))
+    }
     render(){
-        return(
-            <div>
-                <div className='Landingpage-f'>
-                    <div className="grid grid-item1"></div>
-                    <div className="grid grid-item1"></div>
-                    <div className="grid grid-item1"></div>
-                    <div className="grid grid-item1"></div>
-                    <div className="grid grid-item1"></div>
-                    <div className="grid grid-item1"></div>
+        var { isLoaded,items} = this.state;
+        if(!isLoaded){
+            return<div>loading...</div>;
+
+        }
+        else{
+            return (
+                <div className="Landingpage-f">
+                        {items.map(item =>(
+                            // <li key={item.id}>
+                            //     Name:{item.name}|Email:{item.email}
+                            // </li>
+                            <div className='grid'>
+                                <div className='card-container'>
+                                    <div className='card-image'>
+                                          <img src={item.image} alt=""/>
+                                    </div>
+
+                                    <div className='card-Data'>
+                                        <hi>{item.name}</hi>
+
+                                    </div>
+                                </div>
+                            </div>
+                            ))
+                         }
                 </div>
-            </div>
-        )
+            )
+        }
+        // return(
+        //     <div>
+        //         <div className='Landingpage-f'>
+        //             <div className="grid grid-item1"></div>
+        //             <div className="grid grid-item1"></div>
+        //             <div className="grid grid-item1"></div>
+        //             <div className="grid grid-item1"></div>
+        //             <div className="grid grid-item1"></div>
+        //             <div className="grid grid-item1"></div>
+        //         </div>
+        //     </div>
+        // )
     }
     
 }
