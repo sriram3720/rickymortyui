a5f24e479acba2226ed991d2967b1316751a3487
sriram <sriram3720@gmail.com>
2022-09-21 19:44:44 +0530

    Funtional Component

--- src/Components/laundingpage/Landingpage.jsx
+++ src/Components/laundingpage/Landingpage.jsx
@@ -1,90 +1,76 @@
- import React, { Component } from 'react'
- import './landingpg.css';
-class Landingpage extends Component  {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            items:[],
-            isLoaded: false,
-        }           
-    }
+import React, { useEffect, useState } from 'react'
+import './landingpg.css';
 
-    componentDidMount(){
+function Landingpage() {
+
+    const [loaded, setLoaded] = useState(false);
+    const [items,setItems] = useState([]);
+
+    const rickyApi = () => {
         fetch('https://rickandmortyapi.com/api/character').then(res=>res.json()
         .then(res =>{
             console.log(res);
-            this.setState({
-                isLoaded: true,
-                items:res.results,
-            })
+            setLoaded(true);
+            setItems(res.results);
+            
         }))
     }
-    render(){
-        var { isLoaded,items} = this.state;
-        if(!isLoaded){
-            return<div>loading...</div>;
+    useEffect(()=>{
+       rickyApi()
+    })
 
-        }
-        else{
-            return (
-                <div className="Landingpage-f">
-                        {items.map(item =>(
-                            // <li key={item.id}>
-                            //     Name:{item.name}|Email:{item.email}
-                            // </li>
-                            <div className='grid'>
-                                <div className='card-container'>
-                                    <div className='card-image'>
-                                          <img src={item.image} alt=""/>
-                                    </div>
+  return (
+    !loaded ? (<div>loading...</div>) : 
+    <div>
+        <div className="Landingpage-f">
+            {items.map(item =>(
+                // <li key={item.id}>
+                //     Name:{item.name}|Email:{item.email}
+                // </li>
+                <div className='grid'>
+                    <div className='card-container'>
+                        <div className='card-image'>
+                              <img src={item.image} alt=""/>
+                        </div>
 
-                                    <div className='card-Data'>
-                                        <div className="card-name">
-                                            {item.name}
-                                        </div>
-                                        <div className="card-deatail-staus">
-                                            <div 
-                                            style={{ backgroundColor: item.status === "Alive" ? 'green': item.status ==="Dead" ? 'red' : 'grey'}}
-                                            className="card-detail-statusIndicator">
-
-                                            </div>
-                                            <div>
-                                            {item.status}-{item.species}
-                                            </div>
-
-                                        </div>
-                                        <br>
-                                        </br>
-                                        
-
-                                        <div>
-                                           <div className="cardDetail-title"> Last known location:</div>
-                                           <div> {item.location.name}</div>
-                                        </div>
-                                        <br>
-                                        </br>
-                                        <div>
-                                            <div className="cardDetail-title">First seen in:</div>
-                                            <div>{item.origin.name}</div>
-                                             
-
-                                        
-                                            
-                                        </div>
-
-
-                                    </div>
-                                </div>
+                        <div className='card-Data'>
+                            <div className="card-name">
+                                {item.name}
                             </div>
-                            ))
-                         }
-                </div>
-            )
-        }
+                            <div className="card-deatail-staus">
+                                <div 
+                                style={{ backgroundColor: item.status === "Alive" ? 'green': item.status ==="Dead" ? 'red' : 'grey'}}
+                                className="card-detail-statusIndicator">
 
-    }
-    
+                                </div>
+                                <div>
+                                {item.status}-{item.species}
+                                </div>
+
+                            </div>
+                            <br>
+                            </br>
+                            
+
+                            <div>
+                               <div className="cardDetail-title"> Last known location:</div>
+                               <div> {item.location.name}</div>
+                            </div>
+                            <br>
+                            </br>
+                            <div>
+                                <div className="cardDetail-title">First seen in:</div>
+                                <div>{item.origin.name}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                ))
+             }
+    </div>
+    </div>
+  )
 }
 
-export  default Landingpage;
+export default Landingpage;
