05a1687c33cd59955d0ec1b231dff870e9d98e6f
sriraman <sriram3720@gmail.com>
2023-01-06 16:17:58 +0530

    challenge1

--- src/Components/laundingpage/Landingpage.jsx
+++ src/Components/laundingpage/Landingpage.jsx
@@ -1,39 +1,55 @@
-
-import React, { useEffect, useState } from 'react'
-import './landingpg.css';
+import React, { useEffect, useState } from "react";
+import "./landingpg.css";
+import { Input } from "antd";
 
 function Landingpage() {
+  const { Search } = Input;
+  const [searchKey, setSearchKey] = useState("boxer");
+  const onSearch = (value: string) => {
+    setSearchKey(value.toLowerCase());
+  };
 
-    const [loaded, setLoaded] = useState(false);
-    const [items,setItems] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-    const rickyApi = () => {
-        fetch('https://rickandmortyapi.com/api/character').then(res=>res.json()
-        .then(res =>{
-            console.log(res);
-            setLoaded(true);
-            setItems(res.results);
-            
-        }))
-    }
-    useEffect(()=>{
-       rickyApi()
-    },[])
+  const rickyApi = () => {
+    fetch(` https://dog.ceo/api/breed/${searchKey}/images/random/10`).then(
+      (res) =>
+        res.json().then((res) => {
+            if(res.message && res.message.length) {
+                setItems(res.message);
+                setLoaded(true);
+            }
+        })
+    );
+  };
+  useEffect(() => {
+    rickyApi();
+  }, []);
 
-  return (
-    !loaded ? (<div>loading...</div>) : 
+  return !loaded ? (
+    <div>loading...</div>
+  ) : (
     <div>
-        <div className="Landingpage-f">
-            {items.map(item =>(
-                // <li key={item.id}>
-                //     Name:{item.name}|Email:{item.email}
-                // </li>
-                <div className='grid'>
-                    <div className='card-container'>
-                        <div className='card-image'>
-                              <img src={item.image} alt=""/>
-                        </div>
+      <div>
+        <Search
+          className="!w-[400px]"
+          placeholder="input search text"
+          allowClear
+          enterButton="Search"
+          size="large"
+          onSearch={onSearch}
+        />
+      </div>
 
+      <div className="Landingpage-f">
+        {items?.map((item) => (
+          <div className="grid">
+            <div className="">
+              <div className="card-image">
+                <img  className="img " src={item} alt="" />
+              </div>
+              {/* 
                         <div className='card-Data'>
                             <div className="card-name">
                                 {item.name}
@@ -63,14 +79,13 @@ function Landingpage() {
                                 <div className="cardDetail-title">First seen in:</div>
                                 <div>{item.origin.name}</div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                ))
-             }
+                        </div> */}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default Landingpage;
