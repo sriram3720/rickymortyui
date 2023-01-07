0a72c1c028e233c95fbd23acb752ed0591254c38
sriraman <sriram3720@gmail.com>
2023-01-07 19:23:03 +0530

    dog-challenge

--- src/Components/laundingpage/Landingpage.jsx
+++ src/Components/laundingpage/Landingpage.jsx
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./landingpg.css";
-import { Input } from "antd";
+import { Input, Button } from "antd";
+import { HeartOutlined, HeartFilled, HeartTwoTone } from "@ant-design/icons";
+
 
 function Landingpage() {
   const { Search } = Input;
   const [searchKey, setSearchKey] = useState("boxer");
+  const [iconColor, setIconColor] = useState("");
+  const [favorite, setFavorite] = useState([]);
   const onSearch = (value: string) => {
     setSearchKey(value.toLowerCase());
+    rickyApi()
   };
 
   const [loaded, setLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
   const rickyApi = () => {
-    fetch(` https://dog.ceo/api/breed/${searchKey}/images/random/10`).then(
+    fetch(` https://dog.ceo/api/breed/${searchKey}/images/random/18`).then(
       (res) =>
         res.json().then((res) => {
-            if(res.message && res.message.length) {
-                setItems(res.message);
-                setLoaded(true);
-            }
+          if (res.message && res.message.length) {
+            setItems(res.message);
+            setLoaded(true);
+          }
         })
     );
   };
@@ -31,9 +36,10 @@ function Landingpage() {
     <div>loading...</div>
   ) : (
     <div>
+      <h2>Breed</h2>
       <div>
         <Search
-          className="!w-[400px]"
+          style={{ width: "380px", paddingTop: "20px" }}
           placeholder="input search text"
           allowClear
           enterButton="Search"
@@ -47,42 +53,62 @@ function Landingpage() {
           <div className="grid">
             <div className="">
               <div className="card-image">
-                <img  className="img " src={item} alt="" />
+                <img className="img " src={item} alt="" />
+                <Button
+                  style={{
+                    position: "absolute",
+                    left: "125px",
+                    bottom: "1px",
+                    height: "30px",
+                    width: "31px",
+                  }}
+                  icon={<HeartOutlined style={{ color: iconColor }} />}
+                  onClick={() => {
+                    setIconColor("red");
+                    setFavorite([...favorite, item]);
+                   
+                  }}
+                >
+                 
+                </Button>
               </div>
-              {/* 
-                        <div className='card-Data'>
-                            <div className="card-name">
-                                {item.name}
-                            </div>
-                            <div className="card-deatail-staus">
-                                <div 
-                                style={{ backgroundColor: item.status === "Alive" ? 'green': item.status ==="Dead" ? 'red' : 'grey'}}
-                                className="card-detail-statusIndicator">
 
-                                </div>
-                                <div>
-                                {item.status}-{item.species}
-                                </div>
-
-                            </div>
-                            <br>
-                            </br>
-                            
-
-                            <div>
-                               <div className="cardDetail-title"> Last known location:</div>
-                               <div> {item.location.name}</div>
-                            </div>
-                            <br>
-                            </br>
-                            <div>
-                                <div className="cardDetail-title">First seen in:</div>
-                                <div>{item.origin.name}</div>
-                            </div>
-                        </div> */}
+             
             </div>
           </div>
         ))}
+      </div>
+      <div>
+        <h2>Favorite</h2>
+        <div className="Landingpage-f">
+          {favorite?.map((item) => (
+            <div className="grid">
+              <div className="">
+                <div className="card-image">
+                  <img className="img " src={item} alt="" />
+                  <Button
+                    style={{
+                      position: "absolute",
+                      left: "125px",
+                      bottom: "1px",
+                      height: "30px",
+                      width: "31px",
+                    }}
+                    icon={<HeartOutlined style={{ color: iconColor }} />}
+                    onClick={() => {
+                      setIconColor("red");
+                      setFavorite([...favorite, item]);
+                      
+                    }}
+                  >
+                   
+                  </Button>
+                </div>
+
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
